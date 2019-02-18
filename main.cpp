38354ee1f66a88a6e153ad0fdbf5556bc9bdb7e2
Gabriella Ayala <ayalag@b19704.domain>
2019-02-18 13:36:29 -0700

    last minute detail check

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-//  Name      :  Gabriella Ayala        
-//  Student ID:  108427996  
-//  Class     :  CSC 2312           
+//  Name      :  Gabriella Ayala
+//  Student ID:  108427996
+//  Class     :  CSC 2312
 //  Due Date  :  Feb. 18, 2019
 
 #include <iostream>
@@ -9,7 +9,8 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     Rectangle box1;
     Rectangle box2;
 
@@ -18,21 +19,29 @@ int main(){
     cout << "And now the data for Rectangle 2" << endl;
     box2 = box2.initRectangle();
 
-
     cout << "coordinates of box 1 ";
     box1.printCoordinates();
-    cout << "coordinates of box 2 "; 
-    box2.printCoordinates();  
+    cout << "coordinates of box 2 ";
+    box2.printCoordinates();
 
-    if(box1.intersect(box2)){
+    if (box1.intersect(box2))
+    {
         cout << "Box 1 intersects box 2" << endl;
+    }
+    else
+    {
+        cout << "Box 1 doesn't intersect box 2" << endl;
     }
 
     //for demonstration purposes
-    if(box2.intersect(box1)){
+    if (box2.intersect(box1))
+    {
         cout << "Box 2 intersects box 1" << endl;
     }
-
+    else
+    {
+        cout << "Box 2 doesn't intersect box 1" << endl;
+    }
 
     return 0;
 }
